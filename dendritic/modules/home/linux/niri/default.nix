219683b74f37ef35790e2ff5b6d4412b2b337d42
Raihan Adam <hallwack.id@gmail.com>
2026-05-21 02:21:14 +0700

    fix: fixing config using symlink for niri

--- dendritic/modules/home/linux/niri/default.nix
+++ dendritic/modules/home/linux/niri/default.nix
@@ -2,8 +2,12 @@
   ...
 }: {
   flake.modules.homeManager.niri = {
+    config,
+    lib,
+    repoRoot,
     ...
   }: {
-    xdg.configFile."niri".source = ./config;
+    xdg.configFile."niri".source =
+      lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/dendritic/modules/home/linux/niri/config");
   };
 }
