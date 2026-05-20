3f6ffd2e4965ec3007223d49005e6c93a933d7c1
Raihan Adam <hallwack.id@gmail.com>
2026-05-20 23:35:44 +0700

    fix: reorder services in desktop-gnome configuration for clarity

--- dendritic/modules/system/nixos/desktop-gnome.nix
+++ dendritic/modules/system/nixos/desktop-gnome.nix
@@ -2,15 +2,18 @@
   ...
 }: {
   flake.modules.nixos.desktop-gnome = {pkgs, ...}: {
-    services.xserver.enable = true;
     services.xserver = {
-      displayManager.gdm = {
-        enable = true;
-        wayland = true;
-      };
-      desktopManager.gnome.enable = true;
+      enable = true;
+
       xkb.layout = "us";
       xkb.options = "caps:escape";
+    };
+      
+    services.desktopManager.gnome.enable = true;
+
+    services.displayManager.gdm = {
+      enable = true;
+      wayland = true;
     };
 
     services.udev.packages = with pkgs; [
