94aa298d297fc3a968a935297734067745d30bce
Raihan Adam <hallwack.id@gmail.com>
2026-05-21 12:19:25 +0700

    fix: comment out xdg.configFile settings in desktop-hyprland configuration

--- modules/home/linux/desktop-hyprland.nix
+++ modules/home/linux/desktop-hyprland.nix
@@ -7,12 +7,12 @@
     repoRoot,
     ...
   }: {
-    xdg.configFile = {
-      "hypr".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/hyprland");
-      "waybar".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/waybar");
-      "rofi".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/rofi");
-      "mako".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/mako");
-      "swaync".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/swaync");
-    };
+    # xdg.configFile = {
+    #   "hypr".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/hyprland");
+    #   "waybar".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/waybar");
+    #   "rofi".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/rofi");
+    #   "mako".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/mako");
+    #   "swaync".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/swaync");
+    # };
   };
 }
