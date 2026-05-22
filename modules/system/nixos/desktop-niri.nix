f64516868c97a9d355432fcbecfa6384b40642de
Raihan Adam <hallwack.id@gmail.com>
2026-05-22 19:07:11 +0700

    fix: format and align code structure in desktop-niri.nix for improved readability

--- modules/system/nixos/desktop-niri.nix
+++ modules/system/nixos/desktop-niri.nix
@@ -1,25 +1,27 @@
-{
-  ...
+{ ...
 }: {
-  flake.modules.nixos.desktop-niri = {pkgs, ...}: {
-    programs.niri.enable = true;
+  flake.modules.nixos.desktop-niri =
+    { pkgs
+    , ...
+    }: {
+      programs.niri.enable = true;
 
-    security.polkit.enable = true;
-    services.gnome.gnome-keyring.enable = true;
+      security.polkit.enable = true;
+      services.gnome.gnome-keyring.enable = true;
 
-    xdg.portal = {
-      enable = true;
-      xdgOpenUsePortal = true;
-      config.niri = {
-        "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+      xdg.portal = {
+        enable = true;
+        xdgOpenUsePortal = true;
+        config.niri = {
+          "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        };
       };
-    };
 
-    environment.systemPackages = with pkgs; [
-      xwayland-satellite
-      fuzzel
-      apple-cursor
-      brave
-    ];
-  };
+      environment.systemPackages = with pkgs; [
+        xwayland-satellite
+        fuzzel
+        apple-cursor
+        brave
+      ];
+    };
 }
