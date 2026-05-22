abc5eb1fe1d0972702e3229851f85c46ef8399f1
Raihan Adam <hallwack.id@gmail.com>
2026-05-22 19:33:46 +0700

    fix: refactor shell.nix for improved readability and structure

--- modules/home/common/shell.nix
+++ modules/home/common/shell.nix
@@ -1,56 +1,55 @@
-{
-  ...
+{ ...
 }: {
-  flake.modules.homeManager.shell = {
-    config,
-    lib,
-    repoRoot,
-    ...
-  }: {
-    programs.direnv = {
-      enable = true;
-      enableZshIntegration = true;
-      nix-direnv.enable = true;
-    };
-
-    programs.zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
-    programs.starship = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
-    programs.zsh = {
-      enable = true;
-      enableCompletion = false;
-      autosuggestion.enable = false;
-      syntaxHighlighting.enable = true;
-      oh-my-zsh = {
+  flake.modules.homeManager.shell =
+    { config
+    , lib
+    , repoRoot
+    , ...
+    }: {
+      programs.direnv = {
         enable = true;
-        theme = "";
-        plugins = ["git" "vi-mode" "web-search"];
+        enableZshIntegration = true;
+        nix-direnv.enable = true;
       };
-      shellAliases = {
-        vi = "nvim";
-        cls = "clear";
-        cat = "bat";
-        cd = "z";
-        ll = "ls -l";
-        switch = "sudo nixos-rebuild switch --flake ${repoRoot}#hallnet";
+
+      programs.zoxide = {
+        enable = true;
+        enableZshIntegration = true;
       };
-      history = {
-        size = 10000;
-        path = "$HOME/.zsh_history";
+
+      programs.starship = {
+        enable = true;
+        enableZshIntegration = true;
       };
+
+      programs.zsh = {
+        enable = true;
+        enableCompletion = false;
+        autosuggestion.enable = false;
+        syntaxHighlighting.enable = true;
+        oh-my-zsh = {
+          enable = true;
+          theme = "";
+          plugins = [ "git" "vi-mode" "web-search" ];
+        };
+        shellAliases = {
+          vi = "nvim";
+          cls = "clear";
+          cat = "bat";
+          cd = "z";
+          ll = "ls -l";
+          switch = "sudo nixos-rebuild switch --flake ${repoRoot}#hallnet";
+        };
+        history = {
+          size = 10000;
+          path = "$HOME/.zsh_history";
+        };
+      };
+
+      xdg.configFile."starship.toml".source =
+        lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/starship.toml");
+
+      home.file.".zshrc".source =
+        config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/.zshrc";
     };
-
-    xdg.configFile."starship.toml".source =
-      lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/starship.toml");
-
-    home.file.".zshrc".source =
-      lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/.zshrc");
-  };
 }
