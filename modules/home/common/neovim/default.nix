f21083938008a84c0c1b40882c7337ea8cd92f34
Raihan Adam <hallwack.id@gmail.com>
2026-05-22 20:46:15 +0700

    fix: restructure neovim module to enhance configuration management and package setup

--- modules/home/common/neovim/default.nix
+++ modules/home/common/neovim/default.nix
@@ -1,7 +1,33 @@
 {
   ...
 }: {
-  flake.modules.homeManager.neovim.imports = [
-    ./module.nix
-  ];
+  flake.modules.homeManager.neovim = {
+    config,
+    lib,
+    pkgs,
+    repoRoot,
+    ...
+  }: {
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      withPython3 = false;
+      withRuby = false;
+    };
+
+      home.activation.linkNvimConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ -e "$HOME/.config/nvim" ] && [ ! -L "$HOME/.config/nvim" ]; then
+      rm -rf "$HOME/.config/nvim"
+    fi
+    ln -sfn "${repoRoot}/config/nvim" "$HOME/.config/nvim"
+  '';
+
+    home.packages = with pkgs; [
+      ripgrep
+      fd
+      gcc
+    ];
+  };
 }
