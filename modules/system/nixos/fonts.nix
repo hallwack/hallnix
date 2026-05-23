e68c7cf61c66c75e2b2bc70967c0af5d0c033f3a
Raihan Adam <hallwack.id@gmail.com>
2026-05-23 10:48:13 +0700

    fix: add some fonts

--- modules/system/nixos/fonts.nix
+++ modules/system/nixos/fonts.nix
@@ -1,32 +1,38 @@
-{ ...
-}: {
-  flake.modules.nixos.fonts = { pkgs, appleFonts, ... }: {
-    fonts = {
-      enableDefaultPackages = true;
-      fontconfig = {
-        enable = true;
-        defaultFonts = {
-          serif = [
-            "SF Pro"
-          ];
-          sansSerif = [
-            "SF Pro"
-          ];
-          emoji = [
-            "Noto Color Emoji"
-          ];
+{
+  ...
+}:
+{
+  flake.modules.nixos.fonts =
+    { pkgs, appleFonts, ... }:
+    {
+      fonts = {
+        fontconfig.enable = true;
+
+        packages = with pkgs; [
+          nerd-fonts.fira-code
+          nerd-fonts.jetbrains-mono
+          nerd-fonts.geist-mono
+          nerd-fonts.hack
+          nerd-fonts.ubuntu
+          inter
+          appleFonts.sf-pro
+          appleFonts.sf-mono
+          appleFonts.ny
+        ];
+
+        fontconfig = {
+          defaultFonts = {
+            serif = [
+              "SF Pro"
+            ];
+            sansSerif = [
+              "SF Pro"
+            ];
+            emoji = [
+              "Noto Color Emoji"
+            ];
+          };
         };
       };
-      packages = with pkgs; [
-        nerd-fonts.fira-code
-        nerd-fonts.jetbrains-mono
-        nerd-fonts.geist-mono
-        nerd-fonts.hack
-        nerd-fonts.ubuntu
-        inter
-        appleFonts.sf-pro
-        appleFonts.sf-mono
-      ];
     };
-  };
 }
