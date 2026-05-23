3ee1af9d4f70aa3ed25b8f016237822d2910a9aa
Raihan Adam <hallwack.id@gmail.com>
2026-05-23 11:03:56 +0700

    fix: fixing formatting

--- modules/home/common/ghostty/default.nix
+++ modules/home/common/ghostty/default.nix
@@ -1,11 +1,22 @@
-{ ...
-}: {
-  flake.modules.homeManager.ghostty = { pkgs, lib, config, repoRoot, ... }: {
-    home.packages = with pkgs; [
-      ghostty
-    ];
+{
+  ...
+}:
+{
+  flake.modules.homeManager.ghostty =
+    {
+      pkgs,
+      lib,
+      config,
+      repoRoot,
+      ...
+    }:
+    {
+      home.packages = with pkgs; [
+        ghostty
+      ];
 
-    xdg.configFile."ghostty".source =
-      lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/ghostty");
-  };
+      xdg.configFile."ghostty".source = lib.mkForce (
+        config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/ghostty"
+      );
+    };
 }
--- modules/home/common/kitty/default.nix
+++ modules/home/common/kitty/default.nix
@@ -1,11 +1,22 @@
-{ ...
-}: {
-  flake.modules.homeManager.kitty = { pkgs, config, repoRoot, ... }: {
-    home.packages = with pkgs; [
-      kitty
-    ];
+{
+  ...
+}:
+{
+  flake.modules.homeManager.kitty =
+    {
+      pkgs,
+      config,
+      repoRoot,
+      lib,
+      ...
+    }:
+    {
+      home.packages = with pkgs; [
+        kitty
+      ];
 
-    xdg.configFile."kitty".source = config.lib.file.mkOutOfStoreSymlink
-      "${repoRoot}/config/kitty";
-  };
+      xdg.configFile."kitty".source = lib.mkForce (
+        config.lib.file.mkOutOfStoreSymlink "${repoRoot}/config/kitty"
+      );
+    };
 }
