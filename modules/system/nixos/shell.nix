1e1ae2809cf6eed8003db3e86c166cedac016693
Raihan Adam <hallwack.id@gmail.com>
2026-06-17 12:48:20 +0700

    feat: update flake.lock and add nh program configuration in shell.nix

--- modules/system/nixos/shell.nix
+++ modules/system/nixos/shell.nix
@@ -7,6 +7,15 @@
       ...
     }:
     {
+      programs.nh = {
+        enable = true;
+        clean = {
+          enable = true;
+          extraArgs = "--keep-since 7d --keep 3";
+        };
+        flake = "/home/hallwack/hallnix";
+      };
+
       programs.zsh.enable = true;
       programs.direnv.enable = true;
       programs.direnv.nix-direnv.enable = true;
