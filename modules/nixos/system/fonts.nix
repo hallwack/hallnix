88b819e7c2343328a00daae824497086caef3ae8
Raihan Adam <hallwack.id@gmail.com>
2026-07-03 17:57:23 +0700

    fix: fixing fontconfig for nixos system fonts

--- modules/nixos/system/fonts.nix
+++ modules/nixos/system/fonts.nix
@@ -11,40 +11,48 @@
 
   config = lib.mkIf config.hallwack.system.fonts.enable {
 
-    fonts = {
-      fontconfig.enable = true;
+    fonts.fontconfig.packages = with pkgs; [
+      nerd-fonts.geist-mono
+      nerd-fonts.iosevka-term
+      nerd-fonts.iosevka
+      nerd-fonts.symbols-only
 
-      packages = with pkgs; [
-        nerd-fonts.geist-mono
-        nerd-fonts.iosevka-term
-        nerd-fonts.iosevka
-        nerd-fonts.symbols-only
+      inter
 
-        inter
+      appleFonts.sf-pro
+      appleFonts.sf-mono
+      appleFonts.ny
 
-        appleFonts.sf-pro
-        appleFonts.sf-mono
-        appleFonts.ny
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+    ];
 
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-color-emoji
+    fonts.fontconfig.defaultFonts = {
+      serif = [
+        "New York"
+        "Noto Serif"
       ];
+      sansSerif = [
+        "SF Pro Display"
+        "Noto Sans"
+      ];
+      emoji = [
+        "Noto Color Emoji"
+      ];
+    };
 
-      fontconfig = {
-        defaultFonts = {
-          serif = [
-            "New York"
-            "Noto Serif"
-          ];
-          sansSerif = [
-            "SF Pro Display"
-            "Noto Sans"
-          ];
-          emoji = [
-            "Noto Color Emoji"
-          ];
-        };
+    fonts.fontconfig = {
+      enable = true;
+      antialias = true;
+      hinting = {
+        enable = true;
+        autohint = false;
+        style = "full";
+      };
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "light";
       };
     };
   };
