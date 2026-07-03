8f9263d61f7975632b279ca059607e4759b07ede
Raihan Adam <hallwack.id@gmail.com>
2026-07-03 17:32:00 +0700

    feat: add gnupg package in cli for home-manager

--- hosts/hallnet/home.nix
+++ hosts/hallnet/home.nix
@@ -12,6 +12,7 @@
 
   hallwack.cli.devtools.enable = true;
   hallwack.cli.git.enable = true;
+  hallwack.cli.gpg.enable = true;
   hallwack.cli.nodejs.enable = true;
   hallwack.cli.rust.enable = true;
   hallwack.cli.shell.enable = true;
