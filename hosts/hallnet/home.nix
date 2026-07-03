1d1af5e61a376f865aa3819c0d8217f472fcd222
Raihan Adam <hallwack.id@gmail.com>
2026-07-03 16:51:13 +0700

    feat: add rust cli for rust development

--- hosts/hallnet/home.nix
+++ hosts/hallnet/home.nix
@@ -13,6 +13,7 @@
   hallwack.cli.devtools.enable = true;
   hallwack.cli.git.enable = true;
   hallwack.cli.nodejs.enable = true;
+  hallwack.cli.rust.enable = true;
   hallwack.cli.shell.enable = true;
 
   hallwack.desktop.niri.enable = true;
